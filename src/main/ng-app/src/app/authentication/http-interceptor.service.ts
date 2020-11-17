51e0505edbfd6fa378fda38ee460f754c29466f7
VladimirBai <bajjramov.vladimir@gmail.com>
2020-11-17 13:24:23 +0400

    redirect to login page if user is not authorized

--- src/main/ng-app/src/app/authentication/http-interceptor.service.ts
+++ src/main/ng-app/src/app/authentication/http-interceptor.service.ts
@@ -2,20 +2,21 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {AuthenticationService} from "./authentication.service";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpInterceptorService implements HttpInterceptor {
 
-  private urlToSkip = "http://localhost:8889/api/user";
+  private loginUrl = "http://localhost:8889/api/user";
 
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.authenticationService.isUserLoggedIn() && req.url.indexOf('basicauth') === -1
-      && req.url !== this.urlToSkip) {
+    if (this.authenticationService.isUserLoggedIn() && req.url.indexOf('basicauth') === -1) {
       const authReq = req.clone({
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
@@ -24,7 +25,11 @@ export class HttpInterceptorService implements HttpInterceptor {
       });
       return next.handle(authReq);
     } else {
-      return next.handle(req);
+      if (req.url === this.loginUrl) {
+        return next.handle(req);
+      } else {
+        this.router.navigate([""]);
+      }
     }
   }
 
